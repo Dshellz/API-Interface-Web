94462c47f13417c59ea6a0f93baa7fa5d9730e09
Dshellz <davidsanchez38550@gmail.com>
2025-03-28 15:54:44 +0100

    changement apprun

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def delete_user(user_name):
         return ({"success": False, "message": "Utilisateur non trouvé"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
