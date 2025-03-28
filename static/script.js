33f915bcd50ad2197e3a215c90959044494d5606
Dshellz <davidsanchez38550@gmail.com>
2025-03-28 17:23:20 +0100

    lien render

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const name = document.getElementById("name").value;
         const role = document.getElementById("role").value;
 
-        fetch("http://127.0.0.1:5000/add_user", {
+        fetch("https://api-interface-web.onrender.com//add_user", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name: name, role: role })
